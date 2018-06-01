9590e00347a8e26c3a5d8c33aa14c3f5b182210b
David AZOULAY <dazoulay@simplicite.fr>
2018-06-01 23:19:04 +0200

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import Simplicite from 'simplicite';
+
+let app = Simplicite.session({
+	url: 'https://demo.dev.simplicite.io',
+	username: 'website',
+	password: 'simplicite',
+	debug: true
+});
+app.login().then(function(params) {
+	console.log('Logged in as ' + params.username);
+	return app.getGrant(); // next promise
+}, function(reason) {
+	app = null;
+	console.log('Login failed (status: ' + reason.status + ', message: ' + reason.message + ')');
+}).then(function(grant) {
+	if (!app) return;
+	console.log('Hello ' + grant.getLogin() + '!');
+});
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Angular demo';
 }
