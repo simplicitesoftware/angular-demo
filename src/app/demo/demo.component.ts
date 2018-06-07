2ca4febffd83e752adf9f1a5682f245c8efe00d3
David AZOULAY <dazoulay@simplicite.fr>
2018-06-07 13:54:50 +0200

    Updated

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import Simplicite from 'simplicite';
 
 @Component({
@@ -7,15 +7,17 @@ import Simplicite from 'simplicite';
 	styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
-	app = {};
-	user = '';
+	grant = {};
+	products = [];
 
-	constructor() {
+	private debug = false;
+	private app;
+	constructor(private cdr: ChangeDetectorRef) {
 		this.app = Simplicite.session({
 			url: 'https://demo.dev.simplicite.io',
 			username: 'website',
 			password: 'simplicite',
-			debug: false
+			debug: this.debug
 		});
 	}
 
@@ -23,10 +25,16 @@ export class DemoComponent implements OnInit {
 		let self = this;
 		self.app.login().then(function(params) {
 			console.log('Logged in as ' + params.username);
-			self.user = params.username;
 			self.app.getGrant().then(function(grant) {
-				console.log(grant);
-				// Etc.
+				if (self.debug) console.log(grant);
+				self.grant = grant;
+				self.cdr.detectChanges();
+				let prd = self.app.getBusinessObject('DemoProduct');
+				prd.search().then(function(list) {
+					if (self.debug) console.log(list);
+					self.products = list;
+					self.cdr.detectChanges();
+				});
 			});
 		}).fail(function(reason) {
 			console.log('Login failed (status: ' + reason.status + ', message: ' + reason.message + ')');
