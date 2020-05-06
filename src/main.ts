7cdaf6ee161761b22fc4d76576f520c5d87ae558
David AZOULAY <dazoulay@simplicite.fr>
2020-05-06 19:17:20 +0200

    Version 1.0.13

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+/*  ___ _            _ _    _ _
+ * / __(_)_ __  _ __| (_)__(_) |_ ___
+ * \__ \ | '  \| '_ \ | / _| |  _/ -_)
+ * |___/_|_|_|_| .__/_|_\__|_|\__\___|
+ *             |_|
+ * This example is using the Simplicite node.js & browser JavaScript API
+ */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
