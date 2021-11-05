32fff0972dd10c30e2f5a4db6a181c4ed49246ad
David AZOULAY <dazoulay@simplicite.fr>
2021-11-05 13:49:46 +0100

    TSLint fixes

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -2,44 +2,43 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import simplicite from 'simplicite';
 
 @Component({
-	selector: 'app-demo',
-	templateUrl: './demo.component.html',
-	styleUrls: ['./demo.component.css']
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
-	error = '';
-	grant = {};
-	products = [];
+  error = '';
+  grant = {};
+  products = [];
 
-	private debug: boolean = false;
-	private app: any;
-	constructor(private cdr: ChangeDetectorRef) {
-		this.app = simplicite.session({
-			url: 'https://demo.dev.simplicite.io',
-			username: 'website',
-			password: 'simplicite',
-			debug: this.debug
-		});
-	}
+  private debug = false;
+  private app: any;
+  constructor(private cdr: ChangeDetectorRef) {
+    this.app = simplicite.session({
+      url: 'https://demo.dev.simplicite.io',
+      username: 'website',
+      password: 'simplicite',
+      debug: this.debug
+    });
+  }
 
-	ngOnInit() {
-		let self = this;
-		self.app.login().then((params: any) => {
-			console.log('Logged in as ' + params.username);
-			self.app.getGrant().then(function(grant) {
-				if (self.debug) console.log(grant);
-				self.grant = grant;
-				self.cdr.detectChanges();
-				let prd = self.app.getBusinessObject('DemoProduct');
-				prd.search(null, { inlineDocuments: [ 'demoPrdPicture' ] }).then((list: any) => {
-					if (self.debug) console.log(list);
-					self.products = list;
-					self.cdr.detectChanges();
-				});
-			});
-		}).catch((err: any) => {
-			self.error = err.message;
-			self.cdr.detectChanges();
-		});
-	}
+  ngOnInit() {
+    this.app.login().then((params: any) => {
+      console.log('Logged in as ' + params.username);
+      this.app.getGrant().then((grant: any) => {
+        this.app.debug(grant);
+        this.grant = grant;
+        this.cdr.detectChanges();
+        const prd = this.app.getBusinessObject('DemoProduct');
+        prd.search(null, { inlineDocuments: [ 'demoPrdPicture' ] }).then((list: any) => {
+          this.app.debug(list);
+          this.products = list;
+          this.cdr.detectChanges();
+        });
+      });
+    }).catch((err: any) => {
+      this.error = err.message;
+      this.cdr.detectChanges();
+    });
+  }
 }
